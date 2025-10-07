2049c750b3b49f564f4d967148e1dfd6ef07c34b
Abrar <abrar_algh@outlook.sa>
2025-10-07 14:33:32 +0300

    added Lab solution

---
+++ main.py
@@ -0,0 +1,15 @@
+price= 2.99
+quantity= 3
+tax_rate= 0.075
+
+subtotal= price * quantity
+tax= round(subtotal * tax_rate,2)
+total= subtotal + tax
+
+print(f"Price of item: ${price}")
+print(f"Quantity: {quantity}")
+print(f"tax rate: {tax_rate*100}%\n")
+
+print(f"Subtotal: ${subtotal}")
+print(f"Tax: ${tax}")
+print(f"Total: ${total}")
