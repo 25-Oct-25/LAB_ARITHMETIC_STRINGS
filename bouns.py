9de0038964f1625dd1abfe0eecc5e9e2401c0d3c
444101792 <444101792@student.ksu.edu.sa>
2025-10-07 14:34:30 +0300

    solved the bouns

---
+++ bouns.py
@@ -0,0 +1,9 @@
+sentence = "My name is Nawaf Alburaykan I am 21 years old"
+word_in_sentence = "Nawaf"
+print (len(sentence))
+print(sentence.find(word_in_sentence))
+print(sentence.count(word_in_sentence))
+print(sentence.upper())
+print(sentence.lower())
+sentence= sentence.replace(word_in_sentence, "Salem")
+print(sentence[-1])
