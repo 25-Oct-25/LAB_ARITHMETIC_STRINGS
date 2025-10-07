ef663a58897d1a817bb28eb60e1f1cd0dc1f20e3
Abrar <abrar_algh@outlook.sa>
2025-10-07 14:42:42 +0300

    added comment

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+#variables
 price= 2.99
 quantity= 3
 tax_rate= 0.075
 
+#calculating variables
 subtotal= price * quantity
 tax= round(subtotal * tax_rate,2)
 total= subtotal + tax
 
+#print results
 print(f"Price of item: ${price}")
 print(f"Quantity: {quantity}")
 print(f"tax rate: {tax_rate*100}%\n")
