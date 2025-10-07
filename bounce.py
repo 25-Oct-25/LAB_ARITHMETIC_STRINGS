7eb10f4793f763c28f2b6038f6ccdea7acfbac5d
lama <leema442@gmail.com>
2025-10-07 15:17:28 +0300

    Add the lab

---
+++ bounce.py
@@ -0,0 +1,11 @@
+sentace="Hi I am lama saud graduted from PNU at Computer Scinces"
+word="lama"
+
+print("The length of the sentence: "+str(len(sentace)))
+print("the count of word at sentence "+str(sentace.count(word)))
+print("the first index is: "+str(sentace.index(word)))
+print("the number of timr the word appear: "+str(sentace.count(word)))
+print(sentace.upper())
+print(sentace.lower())
+print(sentace.replace(word,"sara"))
+print("the last character of the sentance is: "+sentace[-1])
