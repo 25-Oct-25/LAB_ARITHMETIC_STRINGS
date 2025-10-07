d6ba8c9854926c77b1168488cd69be7b2e35850a
Sana <sanaalboshi77@gmail.com>
2025-10-07 19:05:12 +0300

    sana2

---
+++ main2.py
@@ -0,0 +1,17 @@
+sentence = "My name is Sana and I love learning Python programming daily"
+
+word = "Sana"
+
+print("Length of sentence:", len(sentence))
+
+print("Index of first occurrence of the word:", sentence.find(word))
+
+print("Number of times the word appears:", sentence.count(word))
+
+print("Uppercase sentence:", sentence.upper())
+
+print("Lowercase sentence:", sentence.lower())
+
+print("Replace world:", sentence.replace(word,"Sana Alboshi"))
+
+print("Last character of sentence:", sentence[-1])
