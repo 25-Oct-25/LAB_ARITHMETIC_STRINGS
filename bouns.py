d25d08a54ac5326525418f085f0cce392abe9e84
Brouq <plialq.077@gmail.com>
2025-10-07 15:27:22 +0300

    Add the lab

---
+++ bouns.py
@@ -0,0 +1,11 @@
+sentence = " I love learning python because it is powerful, simple to use every day "
+word = "python"
+
+print ("the lenghth of the sentence: "+str(len (sentence)))
+print ("the count of word at sentence "+str(sentence.count(word)))
+print (" the firts index is: "+str(sentence.index(word)))
+print ("the number of timr the word apper: "+str(sentence.count(word)))
+print (sentence.upper())
+print (sentence.lower())
+print (sentence.replace(word,"brouq"))
+print ("the last character of the sentance is: "+sentence[-1])
